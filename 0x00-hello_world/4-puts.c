4495664b8259fb18e7b12bbeee006c05932f93fb
Clinton Isioma Ofili <clintonagrotekvca@gmail.com>
2023-09-07 06:50:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("\"Programming is like buiding a multilingual puzzle");
+return (0);
+}
